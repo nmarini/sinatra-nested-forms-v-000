169ec3eff69e7143486333ed8b8aebdd82762ee2
Nicolas Marini <nmarini44@gmail.com>
2018-11-18 21:31:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ module FormsLab
       erb :root
     end
 
-    get '/new' do 
+    get '/new' do
 
       erb :new
     end
-      
-    end
+
+
 
     post '/pirates' do
       @pirate = Pirate.new(params)
